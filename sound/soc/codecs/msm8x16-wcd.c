157c2c80fe8eb072e1a67ecac5922f52a3ce821a
flar2 <asegaert@gmail.com>
2017-05-29 14:09:04 -0400

    ASoC: msm8x16-wcd: add speaker gain and prevent reset
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>

--- sound/soc/codecs/msm8x16-wcd.c
+++ sound/soc/codecs/msm8x16-wcd.c
@@ -5912,6 +5912,7 @@ static struct attribute *sound_control_attrs[] = {
 static struct attribute *sound_control_attrs[] = {
 		&headphone_gain_attribute.attr,
 		&mic_gain_attribute.attr,
+		&speaker_gain_attribute.attr,
 		NULL,
 };
 
