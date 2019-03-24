5adc86a364541e361ae17e9d4faecb3e99112f4f
AdityaKevin <adityakevin50@gmail.com>
2019-03-24 14:22:11 +0000

    remove spectrum script

--- AnyKernel/anykernel.sh
+++ AnyKernel/anykernel.sh
@@ -37,26 +37,6 @@ chown -R root:root $ramdisk/*;
 ## AnyKernel install
 dump_boot;
 
-# begin ramdisk changes
-
-# init.rc
-insert_line init.rc "init.spectrum.rc" after "import /init.usb.rc" "import /init.spectrum.rc";
-
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
 
 # end ramdisk changes
 
